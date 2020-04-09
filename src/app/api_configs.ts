d235ebae0344779b07b3ad0bd21d378b00404216
tomwey <tomwey@163.com>
2020-04-09 15:45:28 +0800

    c

---
+++ src/app/api_configs.ts
@@ -0,0 +1,2 @@
+export const API_HOST = "http://b.sr187.me/api/v1";
+export const API_KEY = "7f5cdf133a1b3c7be05cb817d2c2c45d";
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions, URLSearchParams, Response } from '@angular/http';
 import { ToolsService } from './tools.service';
-// import { API_HOST, API_KEY } from '../api_configs';
+import { API_HOST, API_KEY } from '../api_configs';
 import { Md5 } from 'ts-md5/dist/md5';
-import { timeout } from 'rxjs/operators';
-
-const API_SERVER = 'http://erp20-app.heneng.cn:16681';
 
 export interface ResultData {
   code: number;
@@ -35,7 +32,7 @@ export class ApiService {
       this.showLoading(loadingText);
     }
 
-    const url = API_SERVER + '/' + (uri || '');
+    const url = API_HOST + '/' + uri;
 
     const i = new Date().getTime().toString() + (Math.random() * 1000 + 1000).toString();
     const ak = this.generateAccessKey(i);
@@ -86,8 +83,8 @@ export class ApiService {
     if (showLoading) {
       this.showLoading(loadingText);
     }
-    console.log('params:====', params);
-    const url = API_SERVER + '/' + (uri || '');
+
+    const url = API_HOST + '/' + uri;
 
     const i = new Date().getTime().toString() + (Math.random() * 1000 + 1000).toString();
     const ak = this.generateAccessKey(i);
@@ -108,7 +105,6 @@ export class ApiService {
     const requestOptions = new RequestOptions({ headers });
     return new Promise((resolve, reject) => {
       this.http.post(url, JSON.stringify(params), requestOptions)
-        //.pipe(timeout(30000))
         .toPromise()
         .then(resp => {
           this.hideLoading();
@@ -123,46 +119,47 @@ export class ApiService {
         })
         .catch(error => {
           this.hideLoading();
-          // console.log(error);
+
           const err = this.handleError(error);
-          this.tools.showToast(err.message);
-          // console.log(err);
           reject(err);
         });
     });
   } // end post
 
-  // 上传附件
-  upload(body: FormData, loadingText = '正在提交', showLoading = true) {
+
+  POST2(uri, body: FormData, loadingText = '正在提交', showLoading = true) {
     if (showLoading) {
       this.showLoading(loadingText);
     }
 
-    const url = 'http://erp20-app.heneng.cn:16681/upload';
-    // 组装参数
+    const url = API_HOST + '/' + uri;
+
+    const i = new Date().getTime().toString() + (Math.random() * 1000 + 1000).toString();
+    const ak = this.generateAccessKey(i);
+
+    // 参数签名
+    if (localStorage.getItem('token')) {
+      // params.token = localStorage.getItem('token');
+      body.append('token', localStorage.getItem('token'));
+    }
+
+    body.append('i', i.toString());
+    body.append('ak', ak);
 
     // let headers = new Headers({'Content-Type': 'multipart/form-data'});
     return new Promise((resolve, reject) => {
       this.http.post(url, body, null)
-        .pipe(timeout(120000))
         .toPromise()
         .then(resp => {
           this.hideLoading();
-
-          const body = resp.json();
-          if (body.code === '0') {
-            resolve(body);
-          } else {
-            reject(body);
-          }
           // console.log('success');
-          // const result = this.handleSuccess(resp);
-          // if (result.code === '0') {
-          //   resolve(result);
-          //   // resolve({ data: result.data, total: result.total });
-          // } else {
-          //   reject(result);
-          // }
+          const result = this.handleSuccess(resp);
+          if (result.code === 0) {
+            resolve(result);
+            // resolve({ data: result.data, total: result.total });
+          } else {
+            reject(result);
+          }
         })
         .catch(error => {
           this.hideLoading();
@@ -177,36 +174,28 @@ export class ApiService {
   private handleSuccess(resp: Response): any {
     const body = resp.json();
     // console.log(`result: ${JSON.stringify(body)}`);
-    if (body.code === '0') {
-      const rd: ResultData = { code: 0, total: body.rowcount, data: body.data };
+    if (body.code === 0) {
+      const rd: ResultData = { code: 0, total: body.total, data: body.data || {} };
       return rd;
     } else {
-      this.tools.showToast(body.codemsg);
+      this.tools.showToast(body.message);
 
-      // tslint:disable-next-line:radix
-      const errorData: ErrorData = { code: parseInt(body.code.toString()), message: body.codemsg };
+      const errorData: ErrorData = { code: body.code, message: body.message };
       return errorData;
     }
   } // end handle success
 
   private handleError(error: Response | any) {
     let errMsg: string;
-    // console.log(error);
     if (error instanceof Response) {
       const body = error.json() || '';
-      // console.log('body: ', body);
       const err = body.error || JSON.stringify(body);
-      // console.log(err);
-      if (`${err}` === '{"isTrusted":true}') {
-        errMsg = `没有网络连接，请稍后再试~`;
-      } else {
-        errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
-      }
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
     } else {
       errMsg = error.message ? error.message : error.toString();
     }
 
-    // this.tools.showToast(errMsg);
+    this.tools.showToast(errMsg);
 
     const errorData: ErrorData = { code: 500, message: errMsg };
     return errorData;
@@ -229,27 +218,27 @@ export class ApiService {
       signStr += value + ':';
     });
 
-    // signStr += API_KEY;
+    signStr += API_KEY;
 
     return Md5.hashStr(signStr, false).toString();
   }
 
   // 生成MD5
   private generateAccessKey(i): string {
-    return Md5.hashStr('' + i.toString(), false).toString();
+    return Md5.hashStr(API_KEY + i.toString(), false).toString();
   }
 
   private showLoading(loadingText) {
     // console.log(this.count);
-    if (++this.count === 1) {
-      this.tools.showLoading(loadingText, 'lines', 120000);
-    }
+    // if (++this.count === 1) {
+    this.tools.showLoading(loadingText, 'lines', 120000);
+    // }
   }
 
   private hideLoading() {
     // console.log(this.count);
-    if (--this.count === 0) {
-      this.tools.hideLoading().catch(error => { });
-    }
+    // if (--this.count === 0) {
+    this.tools.hideLoading();
+    // }
   }
 }
